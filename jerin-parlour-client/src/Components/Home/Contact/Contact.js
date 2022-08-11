e2fb066da60f24879f02313d32609a8e0190a60e
Imran Hossen <freelancerimran81@gmail.com>
2022-08-11 16:23:40 +0600

    contact page done

--- jerin-parlour-client/src/Components/Home/Contact/Contact.js
+++ jerin-parlour-client/src/Components/Home/Contact/Contact.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className='mid-container'>
+            <h1 className='text-4xl font-bold text-center  py-24'>Let us handle your <br /> project, professionally</h1>
+
+            <form action="" className='w-3/4 mx-auto pb-12'>
+                <div className='text-center mb-5 grid grid-cols-2 gap-5'>
+                    <input type="text" placeholder="Full Name" class="input w-full  " />
+                    <input type="text" placeholder="Last Name" class="input w-full " />
+
+                    <input type="text" placeholder="Full Name" class="input w-full" />
+                    <input type="text" placeholder="Last Name" class="input w-full" />
+                </div>
+                <div className='text-center w-full h-40'>
+                    <textarea class="textarea w-full mr-5 h-full" placeholder="Bio"></textarea>
+                </div>
+                <div className='text-center'>
+                    <button className='btn btn-primary mt-7 '>Send Message</button>
+                </div>
+            </form>
         </div>
     );
 };
--- jerin-parlour-client/src/Components/Home/Home.js
+++ jerin-parlour-client/src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Advertise from './Advertise/Advertise';
 import Banner from './Banner/Banner';
+import Contact from './Contact/Contact';
 import Navbar from './Navbar/Navbar';
 import Services from './Services/Services';
 import Testimonials from './Testimonials/Testimonials';
@@ -15,6 +16,7 @@ const Home = () => {
             <Services />
             <div className='bg-secondary py-36'><Advertise /></div>
             <div className='py-28'><Testimonials /></div>
+            <div className=' bg-secondary'><Contact /></div>
         </div>
     );
 };
